fc960234c6596d41e6ee3562c134d35d5b1130d5
VipexV4B <jeremytheriault23@gmail.com>
2025-09-05 15:12:37 -0400

    commit

--- project/godot_tp1_jeremy/button.gd
+++ project/godot_tp1_jeremy/button.gd
@@ -1,12 +1,11 @@
 extends Control
 
 func _on_button_pressed() -> void:
-	$MusicPlayer.play()
+	$MusicPlayer.play();
 
 
 func _on_CheckButton_toggled(toggled_on: bool):
 	if toggled_on:
-		$MusicPlayer.play()   
-		$MusicPlayer.loop = true
+		$MusicPlayer.play();
 	else:
-		$MusicPlayer.stop()  
+		$MusicPlayer.stop();
--- project/godot_tp1_jeremy/player.gd
+++ project/godot_tp1_jeremy/player.gd
@@ -1,9 +1,19 @@
 extends CharacterBody2D
 
 var speed = 600.0;
+var JUMP_FORCE = -2000.0;
+const GRAVITY = 25.0;
 
-func _process(_delta):
-	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down");
-	
 func _physics_process(delta: float) -> void:
-	move_and_collide(velocity * speed * delta);
+	
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	
+	if not is_on_floor():
+		velocity.y += GRAVITY * speed * delta
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_FORCE
+	
+	move_and_slide()
