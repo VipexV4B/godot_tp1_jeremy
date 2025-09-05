e5fc7751260b49ba58832b8595d5e6a9cca092b1
VipexV4B <jeremytheriault23@gmail.com>
2025-09-05 13:29:38 -0400

    commit

--- project/godot_tp1_jeremy/button.gd
+++ project/godot_tp1_jeremy/button.gd
@@ -2,3 +2,11 @@ extends Control
 
 func _on_button_pressed() -> void:
 	$MusicPlayer.play()
+
+
+func _on_CheckButton_toggled(toggled_on: bool):
+	if toggled_on:
+		$MusicPlayer.play()   
+		$MusicPlayer.loop = true
+	else:
+		$MusicPlayer.stop()  
